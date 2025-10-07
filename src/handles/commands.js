9deddc6ca89d6bdea6adc3bbc2ba37852a0e4ce4
Elder-levi <161390233+Elder-levi@users.noreply.github.com>
2025-10-07 19:57:18 +0000

    novo layout

--- src/handles/commands.js
+++ src/handles/commands.js
@@ -1,7 +1,57 @@
 
 export async function handleCommands(sock,from, text) {
   if(!text) return;
-  const lower = text.toLowerCase(); 
+
+   await sock.sendMessage(from,{
+      text:"Seja-vindo ,somos tal empresa",
+      text:"Nossos ",
+      text:"Academia",
+        
+
+   })
+
+
+
+  const lower = text.toLowerCase().trim();  
+  switch (lower) {
+  case "1":
+       await sock.sendMessage(from,
+        {
+         text:"Academia",
+        })
+  break;
+   
+  case "2":
+      await sock.sendMessage(from,
+        {
+         text:"Personal",
+        })
+  break;
+ 
+  case "3":
+       await sock.sendMessage(from,
+        {
+         text:"Artes-Marciais",
+        })
+  break;  
+  default:
+       await sock.sendMessage(from,
+        {
+         text:"Opaçao invalidade , favor digite opçao valida",
+        })
+  break;
+}
+
+
+
+
+
+
+
+
+
+
+
   if(lower === "ola") {
    await sock.sendMessage(from, {
   text: "Escolha uma opção:",
