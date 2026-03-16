fcc847126918b7b5923b6cffb012bc9ae8258027
Elder-levi <161390233+Elder-levi@users.noreply.github.com>
2026-03-16 16:31:18 +0000

    Continuamdo

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-import {ConectBot} from "./bot.js"
+import express from "express"
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import twilio, { twiml } from "twilio";
+import env from "dotenv"
+ 
 
-ConectBot();
+env.config()
+
+const app = express()
+app.use(express.urlencoded({ extended: false}))
+
+const gentIA = new GoogleGenerativeAI(process.env.CHAVE_API)
+
+const MessagingResponse = twilio.twiml.MessagingResponse;
+
+app.post("/Zapzap" , async (req , res) =>{
+
+    try {
+      
+         const mensagemUser = res.body.body
+
+         const model = GenAI.getGenerativeModel({model: "gemini-1.5-flash"})
+ 
+         const result = await model.generateContent(mensagemUser)
+         const RespAI = (await result.response).text();
+        
+     const twilio = new MessagingResponse();
+
+     twilio.message(RespAI)
+    
+     res.type("text/xml").send(twiml.toString());
+ 
+
+    } catch (error) {
+        
+     console.log("Error ao Envia Mensagem",error)
+    }
+
+})
+
+
+app.listen(3000, ()=>{
+
+    console.log("Bot ligado Na Porta 3000 https://localhost:3000 ")
+})
+
--- src/config/auth.js
+++
@@ -1,6 +0,0 @@
-import { useMultiFileAuthState } from "baileys";
-
-export async function authConfig() {
-    // armazena dados cadastrados do usuario
-    return await useMultiFileAuthState("auth_info")
-}
